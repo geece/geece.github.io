499469f5eddd961e3b6718c38088be76724a9bf3
xinrulyu <sinrulyu@gmail.com>
2019-02-27 01:21:25 +0800

    1.3.1
    ReUpload

---
+++ static/js/about.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){window.onload=function(){lottie.loadAnimation({container:document.getElementById("aboutLogoAnimate"),renderer:"svg",loop:!0,autoplay:!0,path:"js/projectGV.json"})}});
