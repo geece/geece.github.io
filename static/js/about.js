879036bab457126d0c3b05a0f2f027336b2e7b37
xinrulyu <sinrulyu@gmail.com>
2019-03-01 02:52:23 +0800

    1.5.3.1
    About js remove onload

--- static/js/about.js
+++ static/js/about.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     // Body Movin - About Icon
-    window.onload = function () {
-        lottie.loadAnimation({
-            container: document.getElementById('aboutLogoAnimate'),
-            renderer: 'svg',
-            loop: true,
-            autoplay: true,
-            path: 'js/projectGV.json',
-        });
-    }
+    console.log("show about lottie");
+    lottie.loadAnimation({
+        container: document.getElementById('aboutLogoAnimate'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'js/projectGV.json',
+    });
 });
